bcb7aa55e2f2c468d50d3c2eaf85e3e7076723af
Fabian Raeskinet <fabian.raeskinet@avanade.com>
2022-04-21 22:11:32 +0200

    feat: setup dependency injection

---
+++ src/MyDisk.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
+
+namespace MyDisk.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+
+            #region Basic Config
+
+            services.AddControllersWithViews();
+
+            // In production, the React files will be served from this directory
+            services.AddSpaStaticFiles(config => { config.RootPath = "ClientApp/build"; });
+
+            #endregion
+
+            #region Swagger
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1",
+                    new OpenApiInfo
+                    { Title = "MyDisk API v1", Version = "v1", Description = "Different APIs of MyDisk" });
+            });
+            #endregion
+
+            return services;
+        }
+    }
+}
--- src/MyDisk.WebUI/Startup.cs
+++ src/MyDisk.WebUI/Startup.cs
@@ -1,18 +1,21 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MyDisk.Application.Application;
+using MyDisk.Infrastructure;
 
 namespace MyDisk.WebUI
 {
     public class Startup
     {
+        private readonly IConfiguration _configuration;
+
         public Startup(IConfiguration configuration)
         {
-            Configuration = configuration;
+            _configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
@@ -20,15 +23,7 @@ namespace MyDisk.WebUI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
-            services.AddControllersWithViews();
-
-            // In production, the React files will be served from this directory
-            services.AddSpaStaticFiles(configuration =>
-            {
-                configuration.RootPath = "ClientApp/build";
-            });
-            services.AddSwaggerGen();
+            services.AddApplication().AddInfrastructure(_configuration);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
