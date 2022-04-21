38316471aff2db75e3506e2f4f2c63a0983b5651
Fabian Raeskinet <fabian.raeskinet@avanade.com>
2022-04-21 21:36:11 +0200

    Add project files.

---
+++ src/MyDisk.WebUI/Controllers/DiskController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyDisk.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyDisk.WebUI.Controllers
+{
+    [ApiController]
+    [Route("api/")]
+    public class DiskController : ControllerBase
+    {
+
+        private List<Disk> _disks;
+        public DiskController()
+        {
+            _disks = new List<Disk>()
+            {
+                new Disk(){DiskId = "a1", Name = "Joli", ReleaseDate = DateTime.Now}
+            };
+        }
+
+        [HttpGet]
+        [Route("disks")]
+        public IActionResult GetDisks()
+        {
+            return Ok(_disks);
+        }
+    }
+}
